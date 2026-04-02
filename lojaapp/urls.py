82c4991e220ea78d8b0ae1d4e534af2103091e26
AlessandroBatista24 <alessandrobatista24@outlook.com>
2026-04-02 11:30:46 +0200

    modificações na loja virtual

--- lojaapp/urls.py
+++ lojaapp/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from .views import HomeView, ContatoView, ImagemView, TabelasView, SobreView
-
-
+# Adicione CategoriaView aqui embaixo:
+from .views import HomeView, ContatoView, TabelasView, SobreView, CategoriaView, ProdutoDetalheView
 
 app_name = "lojaapp"
 
 urlpatterns = [
-    # Aspas vazias significam a "página inicial" da loja
-    # Precisamos da vírgula depois das aspas
     path("", HomeView.as_view(), name="home"),
-    path("contato/", ContatoView.as_view(), name="contato"),
-    path("imagem/", ImagemView.as_view(), name="imagem"),
+    path("contato/", ContatoView.as_view(), name="contato"),    
     path("tabelas/", TabelasView.as_view(), name="tabelas"),
-    path("sobre/", SobreView.as_view(), name="sobre")
+    path("sobre/", SobreView.as_view(), name="sobre"),
+    path("todos-produtos/", CategoriaView.as_view(), name="todos-produtos"),
+    path("produto/<slug:slug>/", ProdutoDetalheView.as_view(), name="produtodetalhe"),
 ]
--- lojaapp/views.py
+++ lojaapp/views.py
@@ -1,17 +1,44 @@
 from django.views.generic import TemplateView
+from .models import Produto
+from .models import Categoria
 
 class HomeView(TemplateView):
     template_name = "home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["produto_list"] = Produto.objects.all().order_by("-id")
+        return context
     
+class ProdutoView(TemplateView):
+    template_name = "produto.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todascategorias"] = Categoria.objects.all()
+        return context
+    
+class ProdutoDetalheView(TemplateView):
+    template_name = "produto_detalhe.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug'] # Pega o slug da URL
+        context["produto"] = Produto.objects.get(slug=url_slug) # Busca o produto específico
+        return context
 
 class ContatoView(TemplateView):
     template_name = "contato.html"
-
-class ImagemView(TemplateView):
-    template_name = "imagem.html"
 
 class TabelasView(TemplateView):
     template_name = "tabelas.html"
 
 class SobreView(TemplateView):
     template_name = "sobre.html"
+
+class CategoriaView(TemplateView):
+    template_name = "todos_produtos.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todascategorias"] = Categoria.objects.all()
+        return context
+
+    
